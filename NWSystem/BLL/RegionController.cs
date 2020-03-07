ad596334d49780f4e1cb45423b979a85f923ad49
Robbin Law <robbinl@telusplanet.net>
2020-03-06 20:38:06 -0700

    a

---
+++ NWSystem/BLL/RegionController.cs
@@ -0,0 +1,28 @@
+﻿using NWSystem.DAL;
+using NWSystem.ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWSystem.BLL
+{
+    public class RegionController
+    {
+        public List<Region> Regions_List()
+        {
+            using (var context = new NWContext())
+            {
+                return context.Regions.ToList();
+            }
+        }
+        public Region Regions_FindByID(int regionid)
+        {
+            using (var context = new NWContext())
+            {
+                return context.Regions.Find(regionid);
+            }
+        }
+    }
+}
