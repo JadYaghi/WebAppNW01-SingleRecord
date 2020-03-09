d90b929af584c4fc47b05da1e6af07a5d9f530a7
Robbin Law <RobbinL@nait.ca>
2020-03-09 13:39:08 -0600

    a

--- NWSystem/ENTITIES/Region.cs
+++ NWSystem/ENTITIES/Region.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-#region Additional Namespaces
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-#endregion
+
 namespace NWSystem.ENTITIES
 {
     [Table("Region")]
